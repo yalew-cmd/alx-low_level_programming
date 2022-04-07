9685adf18130405a20601e22ec86387ad2e8e545
Yalew Jemberu <yalewjemberu@gmail.com>
2022-04-07 20:11:43 +0300

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
-* This is the preferred style for multi-line
-* comments in C source code.
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
